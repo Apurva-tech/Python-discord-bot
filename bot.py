32be8a438e5fbfbb1328e888044927103079a77c
Jyoti Bisht (Joe) <jyotibisht6114@gmail.com>
2021-09-19 22:44:05 +0530

    added quote REST APIs

--- bot.py
+++ bot.py
@@ -10,4 +10,20 @@ async def faq(ctx, keyword):
     response = keyword
     await ctx.send(response)
 
+def get_qoute():
+    response = requests.get("https://zenquotes.io/api/random")
+    json_data = json.loads(response.text)
+    quote = json_data[0]['q'] + " -" + json_data[0]['a']
+    return (quote)
+    
+@bot.event
+async def on_message(message):
+    await bot.process_commands(message)
+    msg = message.content
+    if message.author == bot.user:
+        return
+    if 'quote' in msg.lower():
+        response = get_qoute()
+        await message.channel.send(response)
+
 bot.run(TOKEN)
